523765c7696b5b589fc3984b15bfb2c21a1ddbc3
Kishan Shiyal <kishan.shiyal@hotmail.co.uk>
2023-03-16 17:40:26 +0000

    More code added into the Paint Calculator, added If and Switch

--- PaintCalculator/src/main/java/org/example/Main.java
+++ PaintCalculator/src/main/java/org/example/Main.java
@@ -12,24 +12,58 @@ public class Main {
 
          */
 
-        float pricePerLitre = 5.99f;
-        float litrePerMetreSquared = 0.20f;
+        // use if else
+        // use switch
+        // different types of paint
+
         float totalLength = 15.45f;
         float totalWidth = 3.50f;
 
-        float totalArea = getArea(totalLength, totalWidth);
-        float totalCost = getTotalCost(totalArea, litrePerMetreSquared, pricePerLitre);
+        String colourCategoryOfPaint = "Pastel"; // Basic, Pastel, Vibrant
+        String typeOfPaint = "Gloss"; // Gloss, Matte, Satin
 
-        System.out.println("£" + Math.round(totalCost));
+        float totalArea = getArea(totalLength, totalWidth);
+        float totalLitres = getTotalLitres(totalArea);
+        float pricePerLitre = getPrice(colourCategoryOfPaint, typeOfPaint);
+        double totalCost = getTotalCost(totalLitres, pricePerLitre);
+        System.out.println("£" + (Math.round(totalCost * 100.0) / 100.0));
+    }
+
+    public static float getTotalLitres (float area){
+        float metreSquaredPerLitre = 15.50f; // Fixed
+        float amount = area/metreSquaredPerLitre;
+        return amount;
     }
 
     public static float getArea (float length, float width){
-
         return (length*width);
     }
 
-    public static float getTotalCost (float area, float litrePerMetreSquared, float pricePerLitre){
+    public static float getPrice (String colourCat, String type){
+        //type of paint
+        float price = 0.00f;
+        switch (colourCat) {
+            case "Gloss":
+                price = 3.99f;
+            case "Matte":
+                price = 4.49f;
+            case "Satin":
+                price = 4.74f;
+            default:
+                price = 0.00f;
+        }
+        //colour category
+        if (type == "Basic") {
+            price *= 1;
+        } else if (type == "Pastel") {
+            price *= 1.25;
+        } else {
+            price *= 1.35;
+        }
+        return (price);
+    }
 
-        return ((area * litrePerMetreSquared)*pricePerLitre);
+    public static float getTotalCost (float totalLitres, float price){
+        return price*totalLitres;
     }
 }
